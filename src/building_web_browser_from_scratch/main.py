db75446a81fc8bd25d551a1b2f05b5ce98a78017
João Gabriel Sampaio de Barros <jgabrielsb2002@gmail.com>
2026-01-28 19:41:48 -0300

    chore

--- src/building_web_browser_from_scratch/main.py
+++ src/building_web_browser_from_scratch/main.py
@@ -1,22 +1,43 @@
-import socket
-from building_web_browser_from_scratch.adapters.requests import SocketHTTPSRequest
 from building_web_browser_from_scratch.core.models import *
+from building_web_browser_from_scratch.adapters.requests import SocketHTTPSRequest
 
-s = SocketHTTPSRequest()
-
+hostname = "browser.engineering"
 request = Request(
     request_line=RequestLine(
         method=MethodEnum.GET,
-        path="/",
-        http_version="HTTP/1.1"
+        http_version="HTTP/1.0",
+        path="/examples/xiyouji.html"
     ),
     headers=[
-        BaseHeader(
-            name="Host", value="example.com"
-        )
+        BaseHeader(name="Host", value=hostname)
     ]
 )
 
-response = s.send_request(request, Destiny(url="example.com", port=443))
+if __name__ == "__main__":
+    response = SocketHTTPSRequest().send_request(input=request, to=Destiny(port=443, hostname=hostname))
+    print(response)
 
-print(response)
+# import tkinter 
+
+# WIDTH = 800
+# HEIGHT = 600
+
+# class Browser:
+#     def __init__(self):
+#         self.window = tkinter.Tk()
+#         self.canvas = tkinter.Canvas(
+#             self.window,
+#             width=WIDTH,
+#             height=HEIGHT
+#         )
+#         self.canvas.pack()
+
+#     def load(self, url):
+#         self.canvas.create_rectangle(10, 20, 400, 300)
+#         self.canvas.create_oval(100, 100, 150, 150)
+#         self.canvas.create_text(200, 150, text="Hi!")
+
+# if __name__ == "__main__":
+#     import sys
+#     Browser().load('hello world!')
+#     tkinter.mainloop()
